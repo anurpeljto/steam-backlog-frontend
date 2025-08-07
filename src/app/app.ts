540eb2e68e0fd6edcc99323a90e4cd5ad92e2009
anurpeljto <anur.peljto@stu.ibu.edu.ba>
2025-08-07 14:39:52 +0200

    Fix dark mode

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSteam } from '@fortawesome/free-brands-svg-icons';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FontAwesomeModule],
+  imports: [RouterOutlet, FontAwesomeModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit{
   faSteam = faSteam;
   protected readonly title = signal('steam-backlog-frontend');
+  isHome: boolean = false;
+  private routerSub!: Subscription;
+
+  constructor(
+    private router: Router
+  ){}
 
   ngOnInit(): void {
+    this.routerSub = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isHome = event.urlAfterRedirects === '/' || event.url === '';
+      });
     document.body.classList.add('cursor-pointing');
     document.addEventListener('mousedown', () => {
       document.body.classList.add('cursor-closed');
